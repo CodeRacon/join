2a71f2cd3c3424f28434f12d4fafcedc4e233670
CodeRacon <michael.buschmann.86@gmail.com>
2024-01-26 11:59:29 +0100

    added includeHTML - function, added jsdoc

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Asynchronously fetches HTML snippets and includes them in the page.
+ *
+ * It looks for elements with the 'w3-include-html' attribute,
+ * fetches the file specified in that attribute, and replaces the innerHTML
+ * of the element with the fetched HTML.
+ *
+ * If the fetch fails, it sets the innerHTML to an error message instead.
+ */
+async function includeHTML() {
+  let includeElements = document.querySelectorAll('[w3-include-html]');
+
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute('w3-include-html');
+    let resp = await fetch(file);
+
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = 'Page not found';
+    }
+  }
+}
