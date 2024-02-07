e5c32621c7b8f221c0aeeec549c038c4ff33bf52
CodeRacon <michael.buschmann.86@gmail.com>
2024-02-07 12:15:45 +0100

    feat: backing up 'contacts' - array

--- js/assets.js
+++ js/assets.js
@@ -18,3 +18,114 @@ const hexColors = [
   '#FFC701',
   '#FFE62B',
 ];
+
+contacts = [
+  {
+    name: 'Kamilla Morgentau',
+    phone: '(741) 762-6527',
+    email: 'k.morgentau@gmail.com',
+    color: '#0038FF',
+  },
+  {
+    name: 'Gerd Förster',
+    phone: '(606) 200-4805',
+    email: 'g.foerster86@gmail.com',
+    color: '#00BEE8',
+  },
+  {
+    name: 'Bernd Steinemann',
+    phone: '(606) 659-4325',
+    email: 'steinemann83@mail.com',
+    color: '#1FD7C1',
+  },
+  {
+    name: 'Sophia Kostas',
+    phone: '(264) 911-2395',
+    email: 's.kostas84@mail.com',
+    color: '#6E52FF',
+  },
+  {
+    name: 'Ahmed Al-Farsi',
+    phone: '(353) 365-9874',
+    email: 'a.alfarsi89@mail.com',
+    color: '#9747FF',
+  },
+  {
+    name: 'Lindsay Orn',
+    phone: '(079) 862-6516',
+    email: 'lorn99@mail.com',
+    color: '#687fc4',
+  },
+  {
+    name: 'Annetta Grant',
+    phone: '(778) 560-8034',
+    email: 'a.grant@mail.com',
+    color: '#FC71FF',
+  },
+  {
+    name: 'Henna Yilmaz',
+    phone: '(494) 694-8019',
+    email: 'h.yilmaz79@gmail.com',
+    color: '#FF4646',
+  },
+  {
+    name: 'Joey Kub',
+    phone: '(227) 955-4546',
+    email: 'jokub74@mail.com',
+    color: '#FF5EB3',
+  },
+  {
+    name: 'Pascale Feeney',
+    phone: '(830) 099-6852',
+    email: 'pas.feeney@mail.com',
+    color: '#FF745E',
+  },
+  {
+    name: 'Lea Stamm',
+    phone: '(004) 386-7068',
+    email: 'lea.stamm98@gmail.com',
+    color: '#FF7A00',
+  },
+  {
+    name: 'Tess Moore',
+    phone: '(549) 753-7594',
+    email: 't.moore99@gmail.com',
+    color: '#FFA35E',
+  },
+  {
+    name: 'Jazmyn Tremblay',
+    phone: '(866) 844-0133',
+    email: 'j.tremblay92@mail.com',
+    color: '#FFBB2B',
+  },
+  {
+    name: 'George Kozey',
+    phone: '(176) 842-2961',
+    email: 'ge-ko@mail.com',
+    color: '#FFC701',
+  },
+  {
+    name: 'Hasan Demir',
+    phone: '(733) 649-6102',
+    email: 'hasan.demir81@mail.com',
+    color: '#9c8900',
+  },
+  {
+    name: 'Kjelt Becker',
+    phone: '(484) 742-4202',
+    email: 'k.becker@gmail.com',
+    color: '#61c39d',
+  },
+  {
+    name: 'Gene Ward',
+    phone: '(876) 110-8650',
+    email: 'g.ward@gmail.com',
+    color: '#1dab93',
+  },
+  {
+    name: 'Mike Skinner',
+    phone: '(758) 519-1073',
+    email: 'm.skinner89@gmail.com',
+    color: '#659c2b',
+  },
+];
