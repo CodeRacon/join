b2c20bdeb0419da8529789a264567003f36e86b3
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-02-07 21:04:29 +0100

    completed structure for assign-to and functionality

---
+++ js/add_task.js
@@ -0,0 +1,12 @@
+function openAndCloseDropDownToAssign() {
+  const dropdownContent = document.getElementById("dropdownContent");
+  const img = document.getElementById("arrowImg");
+
+  if (dropdownContent.style.display !== "block") {
+    dropdownContent.style.display = "block";
+    img.style.transform = "rotate(180deg)";
+  } else {
+    dropdownContent.style.display = "none";
+    img.style.transform = "rotate(0deg)";
+  }
+}
