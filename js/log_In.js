c89fceb2967842fc32fd4337facf436b99026922
Cem20 <cinar.cem@web.de>
2024-02-12 14:56:08 +0100

    Log-In finished

--- js/log_In.js
+++ js/log_In.js
@@ -1,32 +1,51 @@
 let createdAccounts = [];
 let login_value = [];
 let num = 0;
-let dataProperty;  // Verlagerung der dataProperty-Deklaration außerhalb der Funktionen
+let SignUpData = [];
+let logInValue = [];
 
 async function init() {
     await fetchSignUpData();
-  
 }
 
 async function fetchSignUpData() {
     let url = `https://remote-storage.developerakademie.org/item?key=User&token=OLCMKPDCPKF9TQULRK3MARG5U8JK2GGGL5588K0M`;
     let response = await fetch(url);
     createdAccounts = await response.json();
-    dataProperty = createdAccounts.data;  // Aktualisiere dataProperty mit den Daten aus createdAccounts
-
-    // If 'value' is a string, parse it into a JSON array
-        let hombre = JSON.parse(dataProperty['value'])
-    
-
-    console.log(hombre[5]);
+    let dataProperty = JSON.parse(createdAccounts.data.value); 
+    SignUpData = dataProperty;
 }
 
 function checkExistence() {
-    let mail = document.getElementById('login-email');
-    let password = document.getElementById('login-password');  // Korrektur des ID-Namens
+    let mail = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+    if(checkIfValueIsLegit(mail, password) == true){
+        alert('found');
+    }else{
+        alert('dasd');
+        document.getElementById('login-email').style.borderColor = 'red';
+        document.getElementById('login-password').style.borderColor = 'red';
+        document.getElementById('john').classList.remove('dn');
+    }
 }
 
+function checkIfValueIsLegit(mail, password){
+    for (let i = 0; i < SignUpData.length; i++) {
+        if (SignUpData[i]['email'] == mail && SignUpData[i]['pasword'] == password) {
+            return true;
+        }
+    }
+    return false;
+}
 
+function checkBox(){
+    let isCheckedOrNot = document.getElementById('checkedOrNot');
+    isCheckedOrNot.innerHTML = `<img onclick="getBack()" class="imgmo" src="assets/img/icons/general/other/checked.svg" alt="" srcset="">`
+} 
+
+function getBack(){
+    document.getElementById('checkedOrNot').innerHTML = `<img onclick="checkBox()" class="imgmo" src="assets/img/icons/general/other/unchecked.svg" alt="" srcset="">`
+}
 
 // Rufe init() auf, um den Datenabruf zu starten
-init();
+
