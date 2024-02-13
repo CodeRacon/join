ac11b9b6d59ffe8251dbfa9693847b5028e7238f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-02-13 14:08:53 +0100

    added updateHTML to initPage

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ async function initPage() {
   await includeHTML();
   highlightNavLink();
   changeFrameOnNewTab();
+  updateHTML();
 }
 
 /**
@@ -19,17 +20,17 @@ async function initPage() {
  * If the fetch fails, it sets the innerHTML to an error message instead.
  */
 async function includeHTML() {
-  let includeElements = document.querySelectorAll('[w3-include-html]');
+  let includeElements = document.querySelectorAll("[w3-include-html]");
 
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
-    file = element.getAttribute('w3-include-html');
+    file = element.getAttribute("w3-include-html");
     let resp = await fetch(file);
 
     if (resp.ok) {
       element.innerHTML = await resp.text();
     } else {
-      element.innerHTML = 'Page not found';
+      element.innerHTML = "Page not found";
     }
   }
 }
@@ -39,35 +40,35 @@ function goBack() {
 }
 
 function highlightNavLink() {
-  const navLinks = document.querySelectorAll('aside a');
+  const navLinks = document.querySelectorAll("aside a");
   const currentUrl = window.location.pathname;
 
   navLinks.forEach((navLink) => {
-    const href = navLink.getAttribute('href');
+    const href = navLink.getAttribute("href");
     if (currentUrl == href) {
-      navLink.classList.add('visited');
+      navLink.classList.add("visited");
     } else {
-      navLink.classList.remove('visited');
+      navLink.classList.remove("visited");
     }
   });
 }
 
 function changeFrameOnNewTab() {
-  const sideNav = document.getElementById('side-nav');
-  const headerIcons = document.getElementById('corner-icons');
-  const privacyUrl = '/privacy_policy.html';
-  const legalNoticeUrl = '/legal_notice.html';
+  const sideNav = document.getElementById("side-nav");
+  const headerIcons = document.getElementById("corner-icons");
+  const privacyUrl = "/privacy_policy.html";
+  const legalNoticeUrl = "/legal_notice.html";
   const currentUrl = window.location.pathname;
   if (currentUrl === privacyUrl || currentUrl === legalNoticeUrl) {
-    sideNav.classList.toggle('hidden');
-    headerIcons.classList.toggle('hidden');
+    sideNav.classList.toggle("hidden");
+    headerIcons.classList.toggle("hidden");
   }
 }
 
 function openExternalLink(url) {
   const currentUrl = window.location.pathname;
-  const privacyUrl = '/privacy_policy.html';
-  const legalNoticeUrl = '/legal_notice.html';
+  const privacyUrl = "/privacy_policy.html";
+  const legalNoticeUrl = "/legal_notice.html";
   if (currentUrl !== privacyUrl && currentUrl !== legalNoticeUrl) {
     window.open(url);
   } else if (currentUrl === privacyUrl) {
