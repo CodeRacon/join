57849e6915074d440c95ec6f99aea344b81ec7cd
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-03-20 14:44:53 +0100

    removed stashed

--- js/assets.js
+++ js/assets.js
@@ -32,45 +32,11 @@ let hexColors = [
   "#FF005E",
   "#006AFF",
   "#00B8AB",
-<<<<<<< Updated upstream
-  "#b83c3c",
-  "#00BEE8",
-  "#1FD7C1",
-  "#6E52FF",
-  "#9747FF",
-  "#A6C063",
-  "#FC71FF",
-  "#FF4646",
-  "#FF5EB3",
-  "#FF745E",
-  "#FF7A00",
-  "#FFA35E",
-  "#FFBB2B",
-  "#FFC701",
-  "#FFE62B",
-  "#00FF9D",
-  "#00B155",
-  "#85FF00",
-  "#FFD100",
-  "#FFC300",
-  "#FF00E1",
-  "#FF0091",
-  "#0077FF",
-  "#00A6FF",
-  "#FF6A00",
-  "#FF8A00",
-  "#FF0038",
-  "#FF005E",
-  "#006AFF",
-  "#00B8AB",
-=======
->>>>>>> Stashed changes
 ];
 
 startData = {
   users: [
     {
-<<<<<<< Updated upstream
       isRegistered: true,
       isLoggedIn: false,
       userData: {
@@ -177,7 +143,6 @@ startData = {
     {
       isRegistered: true,
       isLoggedIn: false,
-=======
       color: "#659c2b",
       tasks: [
         {
@@ -434,7 +399,6 @@ startData = {
       color: "#2dab6c",
     },
     {
->>>>>>> Stashed changes
       userData: {
         name: "Gerd Förster",
         phone: "(606) 200-4805",
@@ -442,7 +406,6 @@ startData = {
         password: "pw1234",
       },
       color: "#00BEE8",
-<<<<<<< Updated upstream
       tasks: [
         {
           id: 6,
@@ -649,176 +612,7 @@ startData = {
         email: "m.skinner89@gmail.com",
         password: "pw1234",
       },
-  contacts: [
-    {
-      userData: {
-        name: "Sophia Kostas",
-        phone: "(264) 911-2395",
-        email: "s.kostas84@mail.com",
-      },
-      color: "#6E52FF",
-    },
-    {
-      userData: {
-        name: "Bernd Steinemann",
-        phone: "(606) 659-4325",
-        email: "steinemann83@mail.com",
-      },
-      color: "#2dab6c",
-    },
-    {
-      userData: {
-        name: "Gerd Förster",
-        phone: "(606) 200-4805",
-        email: "g.foerster86@gmail.com",
-        password: "pw1234",
-      },
-      color: "#00BEE8",
-    },
-    {
-      userData: {
-        name: "Kamilla Morgentau",
-        phone: "(741) 762-6527",
-        email: "k.morgentau@gmail.com",
-        password: "pw1234",
-      },
-      color: "#b83c3c",
-    },
-    {
-      userData: {
-        name: "Mike Skinner",
-        phone: "(758) 519-1073",
-        email: "m.skinner89@gmail.com",
-        password: "pw1234",
-      },
 
-      color: "#659c2b",
-    },
-    {
-      userData: {
-        name: "Ahmed Al-Farsi",
-        phone: "(353) 365-9874",
-        email: "a.alfarsi89@mail.com",
-      },
-      color: "#9747FF",
-    },
-    {
-      userData: {
-        name: "Lindsay Orn",
-        phone: "(079) 862-6516",
-        email: "lorn99@mail.com",
-      },
-      color: "#687fc4",
-    },
-    {
-      userData: {
-        name: "Annetta Grant",
-        phone: "(778) 560-8034",
-        email: "a.grant@mail.com",
-      },
-      color: "#FC71FF",
-    },
-    {
-      userData: {
-        name: "Henna Yilmaz",
-        phone: "(494) 694-8019",
-        email: "h.yilmaz79@gmail.com",
-      },
-      color: "#FF4646",
-    },
-    {
-      userData: {
-        name: "Joey Kub",
-        phone: "(227) 955-4546",
-        email: "jokub74@mail.com",
-      },
-      color: "#FF5EB3",
-    },
-    {
-      userData: {
-        name: "Pascale Feeney",
-        phone: "(830) 099-6852",
-        email: "pas.feeney@mail.com",
-      },
-      color: "#FF745E",
-    },
-    {
-      userData: {
-        name: "Lea Stamm",
-        phone: "(004) 386-7068",
-        email: "lea.stamm98@gmail.com",
-      },
-      color: "#FF7A00",
-    },
-    {
-      userData: {
-        name: "Tess Moore",
-        phone: "(549) 753-7594",
-        email: "t.moore99@gmail.com",
-      },
-      color: "#FFA35E",
-    },
-    {
-      userData: {
-        name: "Jazmyn Tremblay",
-        phone: "(866) 844-0133",
-        email: "j.tremblay92@mail.com",
-      },
-      color: "#FFBB2B",
-    },
-    {
-      userData: {
-        name: "George Kozey",
-        phone: "(176) 842-2961",
-        email: "ge-ko@mail.com",
-      },
-      color: "#FFC701",
-    },
-    {
-      userData: {
-        name: "Hasan Demir",
-        phone: "(733) 649-6102",
-        email: "hasan.demir81@mail.com",
-      },
-      color: "#9c8900",
-    },
-    {
-      userData: {
-        name: "Kjelt Becker",
-        phone: "(484) 742-4202",
-        email: "k.becker@gmail.com",
-      },
-      color: "#61c39d",
-    },
-    {
-      userData: {
-        name: "Gene Ward",
-        phone: "(876) 110-8650",
-        email: "g.ward@gmail.com",
-      },
-      color: "#1dab93",
-    },
-  ],
-=======
-    },
-    {
-      userData: {
-        name: "Kamilla Morgentau",
-        phone: "(741) 762-6527",
-        email: "k.morgentau@gmail.com",
-        password: "pw1234",
-      },
-      color: "#b83c3c",
-    },
-    {
-      userData: {
-        name: "Mike Skinner",
-        phone: "(758) 519-1073",
-        email: "m.skinner89@gmail.com",
-        password: "pw1234",
-      },
-
->>>>>>> Stashed changes
       color: "#659c2b",
     },
     {
@@ -929,15 +723,8 @@ startData = {
 };
 
 let loginData = [];
-<<<<<<< Updated upstream
-let loginData = [];
 
 async function storeLoginData() {
-async function storeLoginData() {
-=======
-
-async function storeLoginData() {
->>>>>>> Stashed changes
   await setItem("LogInData", JSON.stringify(loginData));
 }
 /**
@@ -945,31 +732,11 @@ async function storeLoginData() {
  */
 function storeStartData() {
   setItem("startData", JSON.stringify(startData));
-<<<<<<< Updated upstream
-  setItem("startData", JSON.stringify(startData));
-}
-async function getData() {
-  await getItem("startData");
-=======
->>>>>>> Stashed changes
 }
 async function getData() {
   await getItem("startData");
 }
-
 let spezLogInData = [];
-<<<<<<< Updated upstream
-let spezLogInData = [];
-=======
->>>>>>> Stashed changes
-
 async function storeSpezLogInData() {
   await setItem("LogInData", JSON.stringify(spezLogInData));
 }
-<<<<<<< Updated upstream
-
-async function storeSpezLogInData() {
-  await setItem("LogInData", JSON.stringify(spezLogInData));
-}
-=======
->>>>>>> Stashed changes
