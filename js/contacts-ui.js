d466e70ba17c4d5f43f88336c08fdab62fc1db8b
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-05 17:14:05 +0200

    refactor: moved logic for form-validation from contacts.js into contacts-ui.js

--- js/contacts-ui.js
+++ js/contacts-ui.js
@@ -3,11 +3,11 @@
  * screen width is less than or equal to 607px.
  */
 function setMobileLayout() {
-  const screenWidth = window.innerWidth;
-  const contactDetails = document.getElementById('contact-details');
-  if (screenWidth <= 607) {
-    contactDetails.classList.add('d-none');
-  }
+	const screenWidth = window.innerWidth;
+	const contactDetails = document.getElementById('contact-details');
+	if (screenWidth <= 607) {
+		contactDetails.classList.add('d-none');
+	}
 }
 
 /**
@@ -15,13 +15,13 @@ function setMobileLayout() {
  * based on the window width, to hide/show it responsively.
  */
 window.addEventListener('resize', () => {
-  const screenWidth = window.innerWidth;
-  const contactDetails = document.getElementById('contact-details');
-  if (screenWidth <= 607) {
-    contactDetails.classList.add('d-none');
-  } else {
-    contactDetails.classList.remove('d-none');
-  }
+	const screenWidth = window.innerWidth;
+	const contactDetails = document.getElementById('contact-details');
+	if (screenWidth <= 607) {
+		contactDetails.classList.add('d-none');
+	} else {
+		contactDetails.classList.remove('d-none');
+	}
 });
 
 /**
@@ -33,11 +33,11 @@ window.addEventListener('resize', () => {
  * @param {number} i - The index of the clicked contact list item
  */
 function swipeInContactInfoBox() {
-  const contactInfoBox = document.getElementById('contact-info-box');
-  contactInfoBox.classList.remove('slide-in', 'd-none');
-  setTimeout(() => {
-    contactInfoBox.classList.add('slide-in');
-  }, 0);
+	const contactInfoBox = document.getElementById('contact-info-box');
+	contactInfoBox.classList.remove('slide-in', 'd-none');
+	setTimeout(() => {
+		contactInfoBox.classList.add('slide-in');
+	}, 0);
 }
 
 /**
@@ -46,13 +46,13 @@ function swipeInContactInfoBox() {
  * if the screen width is less than or equal to 607px.
  */
 function showContactInfoBoxMobile() {
-  const contactDetails = document.getElementById('contact-details');
-  const contactInfoBox = document.getElementById('contact-info-box');
-  const screenWidth = window.innerWidth;
-  if (screenWidth <= 607) {
-    contactDetails.classList.remove('d-none');
-    contactInfoBox.classList.remove('d-none');
-  }
+	const contactDetails = document.getElementById('contact-details');
+	const contactInfoBox = document.getElementById('contact-info-box');
+	const screenWidth = window.innerWidth;
+	if (screenWidth <= 607) {
+		contactDetails.classList.remove('d-none');
+		contactInfoBox.classList.remove('d-none');
+	}
 }
 
 /**
@@ -62,13 +62,13 @@ function showContactInfoBoxMobile() {
  * This is called when the user closes the popup by clicking the overlay.
  */
 function leaveMobileContactInfoBox() {
-  const contactDetails = document.getElementById('contact-details');
-  const contactInfoBox = document.getElementById('contact-info-box');
-  const allContactItems = document.querySelectorAll('.contact-list-item');
-  allContactItems.forEach((item) => item.classList.remove('selected'));
-  contactDetails.classList.add('d-none');
-  contactInfoBox.classList.add('d-none');
-  scrollNewContactToTop(1);
+	const contactDetails = document.getElementById('contact-details');
+	const contactInfoBox = document.getElementById('contact-info-box');
+	const allContactItems = document.querySelectorAll('.contact-list-item');
+	allContactItems.forEach((item) => item.classList.remove('selected'));
+	contactDetails.classList.add('d-none');
+	contactInfoBox.classList.add('d-none');
+	scrollNewContactToTop(1);
 }
 
 let menuOpen = false;
@@ -81,10 +81,10 @@ let menuOpen = false;
  * @param {Event} event - The click event
  */
 function handleClickOutside(event) {
-  const editMenu = document.getElementById('edit-menu');
-  if (!editMenu.contains(event.target)) {
-    closeMobileEditMenu();
-  }
+	const editMenu = document.getElementById('edit-menu');
+	if (!editMenu.contains(event.target)) {
+		closeMobileEditMenu();
+	}
 }
 
 /**
@@ -94,14 +94,14 @@ function handleClickOutside(event) {
  * Handles menuOpen state.
  */
 function toggleMobileEditMenu() {
-  menuOpen = !menuOpen;
-  if (menuOpen) {
-    openMobileEditMenu();
-    document.addEventListener('click', handleClickOutside);
-  } else {
-    closeMobileEditMenu();
-    document.removeEventListener('click', handleClickOutside);
-  }
+	menuOpen = !menuOpen;
+	if (menuOpen) {
+		openMobileEditMenu();
+		document.addEventListener('click', handleClickOutside);
+	} else {
+		closeMobileEditMenu();
+		document.removeEventListener('click', handleClickOutside);
+	}
 }
 
 /**
@@ -114,17 +114,17 @@ function toggleMobileEditMenu() {
  * This handles the initial opening transition animation of the mobile edit menu.
  */
 function openMobileEditMenu() {
-  const editMenu = document.getElementById('edit-menu');
-  const editMenuBtn = document.getElementById('edit-user-btn');
-  const bodyElement = document.getElementById('contacts-body');
-  if (editMenu.classList.contains('d-none')) {
-    editMenu.classList.replace('em-off', 'em-on');
-    setTimeout(() => {
-      editMenu.classList.toggle('d-none');
-      editMenuBtn.classList.toggle('d-none');
-      bodyElement.classList.add('menu-open');
-    }, 125);
-  }
+	const editMenu = document.getElementById('edit-menu');
+	const editMenuBtn = document.getElementById('edit-user-btn');
+	const bodyElement = document.getElementById('contacts-body');
+	if (editMenu.classList.contains('d-none')) {
+		editMenu.classList.replace('em-off', 'em-on');
+		setTimeout(() => {
+			editMenu.classList.toggle('d-none');
+			editMenuBtn.classList.toggle('d-none');
+			bodyElement.classList.add('menu-open');
+		}, 125);
+	}
 }
 
 /**
@@ -138,18 +138,18 @@ function openMobileEditMenu() {
  * This handles the closing transition animation of the mobile edit menu.
  */
 function closeMobileEditMenu() {
-  const editMenu = document.getElementById('edit-menu');
-  const editMenuBtn = document.getElementById('edit-user-btn');
-  const bodyElement = document.getElementById('contacts-body');
-  if (!editMenu.classList.contains('d-none')) {
-    editMenu.classList.replace('em-on', 'em-off');
-    setTimeout(() => {
-      editMenu.classList.toggle('d-none');
-      editMenuBtn.classList.toggle('d-none');
-      bodyElement.classList.remove('menu-open');
-    }, 125);
-    menuOpen = false;
-  }
+	const editMenu = document.getElementById('edit-menu');
+	const editMenuBtn = document.getElementById('edit-user-btn');
+	const bodyElement = document.getElementById('contacts-body');
+	if (!editMenu.classList.contains('d-none')) {
+		editMenu.classList.replace('em-on', 'em-off');
+		setTimeout(() => {
+			editMenu.classList.toggle('d-none');
+			editMenuBtn.classList.toggle('d-none');
+			bodyElement.classList.remove('menu-open');
+		}, 125);
+		menuOpen = false;
+	}
 }
 
 /**
@@ -159,15 +159,15 @@ function closeMobileEditMenu() {
  * and replaces the 'overlay-off' class with 'overlay-on' on the overlay.
  */
 function openAddContactDB() {
-  const overlay = document.getElementById('overlay');
-  const addContactBox = document.getElementById('add-contact-db');
+	const overlay = document.getElementById('overlay');
+	const addContactBox = document.getElementById('add-contact-db');
 
-  addContactBox.classList.remove('d-none');
-  addContactBox.classList.replace('box-slide-out', 'box-slide-in');
-  overlay.classList.remove('d-none');
-  overlay.classList.replace('overlay-off', 'overlay-on');
-  resetErrorFeedback('add');
-  resetErrorMessage('add');
+	addContactBox.classList.remove('d-none');
+	addContactBox.classList.replace('box-slide-out', 'box-slide-in');
+	overlay.classList.remove('d-none');
+	overlay.classList.replace('overlay-off', 'overlay-on');
+	resetErrorFeedback('add');
+	resetErrorMessage('add');
 }
 
 /**
@@ -176,20 +176,20 @@ function openAddContactDB() {
  * a 350ms delay.
  */
 function closeAddContactDB() {
-  const contactName = document.getElementById('add-contact-name');
-  const contactEmail = document.getElementById('add-contact-email');
-  const contactPhone = document.getElementById('add-contact-phone');
-  const overlay = document.getElementById('overlay');
-  const addContactBox = document.getElementById('add-contact-db');
-  contactName.value = '';
-  contactEmail.value = '';
-  contactPhone.value = '';
-  addContactBox.classList.replace('box-slide-in', 'box-slide-out');
-  overlay.classList.replace('overlay-on', 'overlay-off');
-  setTimeout(() => {
-    addContactBox.classList.add('d-none');
-    overlay.classList.add('d-none');
-  }, 350);
+	const contactName = document.getElementById('add-contact-name');
+	const contactEmail = document.getElementById('add-contact-email');
+	const contactPhone = document.getElementById('add-contact-phone');
+	const overlay = document.getElementById('overlay');
+	const addContactBox = document.getElementById('add-contact-db');
+	contactName.value = '';
+	contactEmail.value = '';
+	contactPhone.value = '';
+	addContactBox.classList.replace('box-slide-in', 'box-slide-out');
+	overlay.classList.replace('overlay-on', 'overlay-off');
+	setTimeout(() => {
+		addContactBox.classList.add('d-none');
+		overlay.classList.add('d-none');
+	}, 350);
 }
 
 /**
@@ -198,15 +198,15 @@ function closeAddContactDB() {
  * and swapping the 'slide-in' and 'slide-out' classes to animate the slide effect.
  */
 function showContactSuccess() {
-  const successBox = document.getElementById('contact-success');
-  successBox.classList.toggle('d-none');
-  successBox.classList.replace('slide-out', 'slide-in');
-  setTimeout(() => {
-    successBox.classList.replace('slide-in', 'slide-out');
-  }, 2000);
-  setTimeout(() => {
-    successBox.classList.toggle('d-none');
-  }, 2150);
+	const successBox = document.getElementById('contact-success');
+	successBox.classList.toggle('d-none');
+	successBox.classList.replace('slide-out', 'slide-in');
+	setTimeout(() => {
+		successBox.classList.replace('slide-in', 'slide-out');
+	}, 2000);
+	setTimeout(() => {
+		successBox.classList.toggle('d-none');
+	}, 2150);
 }
 
 /**
@@ -216,21 +216,21 @@ function showContactSuccess() {
  * @param {number} i - The index of the contact to edit
  */
 function openEditContactDB(i) {
-  currentContact = formattedContactList[i];
-  const overlay = document.getElementById('overlay');
-  const editContactBox = document.getElementById('edit-contact-db');
-  const contactName = document.getElementById('edit-contact-name');
-  const contactEmail = document.getElementById('edit-contact-email');
-  const contactPhone = document.getElementById('edit-contact-phone');
-  contactName.value = currentContact.userData.name;
-  contactEmail.value = currentContact.userData.email;
-  contactPhone.value = currentContact.userData.phone;
-  editContactBox.classList.remove('d-none');
-  editContactBox.classList.replace('box-slide-out', 'box-slide-in');
-  overlay.classList.remove('d-none');
-  overlay.classList.replace('overlay-off', 'overlay-on');
-  updateUserIconInDB(i);
-  resetEditUI();
+	currentContact = formattedContactList[i];
+	const overlay = document.getElementById('overlay');
+	const editContactBox = document.getElementById('edit-contact-db');
+	const contactName = document.getElementById('edit-contact-name');
+	const contactEmail = document.getElementById('edit-contact-email');
+	const contactPhone = document.getElementById('edit-contact-phone');
+	contactName.value = currentContact.userData.name;
+	contactEmail.value = currentContact.userData.email;
+	contactPhone.value = currentContact.userData.phone;
+	editContactBox.classList.remove('d-none');
+	editContactBox.classList.replace('box-slide-out', 'box-slide-in');
+	overlay.classList.remove('d-none');
+	overlay.classList.replace('overlay-off', 'overlay-on');
+	updateUserIconInDB(i);
+	resetEditUI();
 }
 
 /**
@@ -239,11 +239,11 @@ function openEditContactDB(i) {
  * the mobile edit menu after a small delay.
  */
 function resetEditUI() {
-  resetErrorFeedback('edit');
-  resetErrorMessage('edit');
-  setTimeout(() => {
-    closeMobileEditMenu();
-  }, 250);
+	resetErrorFeedback('edit');
+	resetErrorMessage('edit');
+	setTimeout(() => {
+		closeMobileEditMenu();
+	}, 250);
 }
 
 /**
@@ -251,20 +251,20 @@ function resetEditUI() {
  * overlay and edit contact box out, and hiding them after the animation.
  */
 function closeEditContactDB() {
-  const contactName = document.getElementById('edit-contact-name');
-  const contactEmail = document.getElementById('edit-contact-email');
-  const contactPhone = document.getElementById('edit-contact-phone');
-  const overlay = document.getElementById('overlay');
-  const editContactBox = document.getElementById('edit-contact-db');
-  contactName.value = '';
-  contactEmail.value = '';
-  contactPhone.value = '';
-  editContactBox.classList.replace('box-slide-in', 'box-slide-out');
-  overlay.classList.replace('overlay-on', 'overlay-off');
-  setTimeout(() => {
-    editContactBox.classList.add('d-none');
-    overlay.classList.add('d-none');
-  }, 350);
+	const contactName = document.getElementById('edit-contact-name');
+	const contactEmail = document.getElementById('edit-contact-email');
+	const contactPhone = document.getElementById('edit-contact-phone');
+	const overlay = document.getElementById('overlay');
+	const editContactBox = document.getElementById('edit-contact-db');
+	contactName.value = '';
+	contactEmail.value = '';
+	contactPhone.value = '';
+	editContactBox.classList.replace('box-slide-in', 'box-slide-out');
+	overlay.classList.replace('overlay-on', 'overlay-off');
+	setTimeout(() => {
+		editContactBox.classList.add('d-none');
+		overlay.classList.add('d-none');
+	}, 350);
 }
 
 /**
@@ -273,11 +273,129 @@ function closeEditContactDB() {
  * the appropriate one.
  */
 function closeDialogueBox() {
-  const editContactBox = document.getElementById('edit-contact-db');
-  const addContactBox = document.getElementById('add-contact-db');
-  if (editContactBox.classList.contains('d-none')) {
-    closeAddContactDB();
-  } else if (addContactBox.classList.contains('d-none')) {
-    closeEditContactDB();
-  }
+	const editContactBox = document.getElementById('edit-contact-db');
+	const addContactBox = document.getElementById('add-contact-db');
+	if (editContactBox.classList.contains('d-none')) {
+		closeAddContactDB();
+	} else if (addContactBox.classList.contains('d-none')) {
+		closeEditContactDB();
+	}
+}
+
+/**
+ * Validates the form for adding or editing a contact.
+ * Checks if name, email, and phone are valid.
+ * If valid, calls createContact() or editContact() depending
+ * which form is been validated.
+ * Returns false if validation fails.
+ */
+function validateForm(identifier) {
+	const isValidName = validateName(identifier);
+	const isValidEmail = validateEmail(identifier);
+	const isValidPhone = validatePhone(identifier);
+	if (isValidName && isValidEmail && isValidPhone) {
+		if (identifier === 'add') {
+			createContact();
+		} else if (identifier === 'edit') {
+			editContact();
+		}
+	}
+	return false;
+}
+
+/**
+ * Validates the contact name input field.
+ * Checks if the name matches the required pattern and is not empty.
+ * Adds/removes error class on input container and displays error message if invalid.
+ * Returns true if valid, false if invalid.
+ */
+function validateName(identifier) {
+	const nameInputCont = document.getElementById('input--name-' + identifier);
+	const inputName = document.getElementById(identifier + '-contact-name');
+	const nameError = document.getElementById('name-error-' + identifier);
+	const validNamePattern = /^[a-zA-Z-]+ [a-zA-Z-]+ ?[a-zA-Z-]+?$/;
+	if (
+		!validNamePattern.test(inputName.value) ||
+		inputName.value.trim() === ''
+	) {
+		nameInputCont.classList.add('invalid');
+		nameError.textContent = '*Please enter first- and surname.';
+		return false;
+	} else {
+		nameInputCont.classList.remove('invalid');
+		nameError.textContent = '';
+		return true;
+	}
+}
+
+/**
+ * Validates the email input field.
+ * Checks if email is in valid format and not empty.
+ * Adds/removes error class on input container and displays error message if invalid.
+ * Returns true if valid, false if invalid.
+ */
+function validateEmail(identifier) {
+	const emailInputCont = document.getElementById('input--email-' + identifier);
+	const inputEmail = document.getElementById(identifier + '-contact-email');
+	const emailError = document.getElementById('mail-error-' + identifier);
+	if (!inputEmail.value.includes('@') || inputEmail.value.trim() === '') {
+		emailInputCont.classList.add('invalid');
+		emailError.textContent = '*Please enter a valid email address.';
+		return false;
+	} else {
+		emailInputCont.classList.remove('invalid');
+		emailError.textContent = '';
+		return true;
+	}
+}
+
+/**
+ * Validates the phone number input field.
+ * Checks if the phone number contains only digits and is at least 9 digits long.
+ * Adds/removes error class on input container and displays error message if invalid.
+ * Returns true if valid, false if invalid.
+ */
+function validatePhone(identifier) {
+	const phoneInputCont = document.getElementById('input--phone-' + identifier);
+	const inputPhone = document.getElementById(identifier + '-contact-phone');
+	const phoneError = document.getElementById('phone-error-' + identifier);
+	const validPhonePattern = /^[+()-9\s]+$/;
+	if (
+		!validPhonePattern.test(inputPhone.value) ||
+		inputPhone.value.trim() === ''
+	) {
+		phoneInputCont.classList.add('invalid');
+		phoneError.textContent = '*Please enter a valid phone number.';
+		return false;
+	} else {
+		phoneInputCont.classList.remove('invalid');
+		phoneError.textContent = '';
+		return true;
+	}
+}
+
+/**
+ * Removes any invalid error styling from the name, email, and phone input
+ * containers for the contact form depending on the given identifier.
+ */
+function resetErrorFeedback(identifier) {
+	const nameInputCont = document.getElementById('input--name-' + identifier);
+	const emailInputCont = document.getElementById('input--email-' + identifier);
+	const phoneInputCont = document.getElementById('input--phone-' + identifier);
+	nameInputCont.classList.remove('invalid');
+	emailInputCont.classList.remove('invalid');
+	phoneInputCont.classList.remove('invalid');
+}
+
+/**
+ * Removes any error messages from the name, email, and phone
+ * elements for the contact form depending on the given identifier.
+ */
+function resetErrorMessage(identifier) {
+	const nameError = document.getElementById('name-error-' + identifier);
+	const emailError = document.getElementById('mail-error-' + identifier);
+	const phoneError = document.getElementById('phone-error-' + identifier);
+	nameError.textContent = '';
+	emailError.textContent = '';
+	phoneError.textContent = '';
 }
