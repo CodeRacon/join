a95911f5b7d63ae2227329950c26bd931f0c025c
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-11 21:29:58 +0200

    refactor: synthax-highlighting for html-code

--- js/add_task_templates.js
+++ js/add_task_templates.js
@@ -6,22 +6,22 @@
  * @returns {string} The HTML string for the single contact element.
  */
 function showContactsToAssignHTML(i, element) {
-  return `
-      <div id="single-contact${i}" class="single-contact" onclick="getAssignedContacts()">
-        <label for="option${i}" class="label-layout">
-          <input
-            type="checkbox"
-            class="custom-checkbox"
-            id="option${i}"
-            value="${element["userData"]["name"]}"
-            onchange="changeCheckboxColor(${i})"
-          />
-          ${element["userData"]["name"]}
-        </label>
-        <br />
-        ${createContactInitials(element)}
-      </div>
-		`;
+	return /*html*/ `
+    <div id="single-contact${i}" class="single-contact" onclick="getAssignedContacts()">
+      <label for="option${i}" class="label-layout">
+        <input
+          type="checkbox"
+          class="custom-checkbox"
+          id="option${i}"
+          value="${element['userData']['name']}"
+          onchange="changeCheckboxColor(${i})"
+        />
+        ${element['userData']['name']}
+      </label>
+      <br />
+      ${createContactInitials(element)}
+    </div>
+	`;
 }
 
 /**
@@ -32,43 +32,43 @@ function showContactsToAssignHTML(i, element) {
  * @returns {string} The HTML string for the single contact element.
  */
 function filterContactsToAssignHTML(i, element) {
-  return `
-  <div id="single-contact${i}" class="single-contact" onclick="getAssignedContacts()">
-    <label for="option${i}" class="label-layout">
-      <input
-        type="checkbox"
-        class="custom-checkbox"
-        id="option${i}"
-        value="${element["userData"]["name"]}"
-        onchange="changeCheckboxColor(${i})"
-      />
-      ${element["userData"]["name"]}
-    </label>
-    <br />
-    ${createContactInitials(element)}
-  </div>
-`;
+	return /*html*/ `
+    <div id="single-contact${i}" class="single-contact" onclick="getAssignedContacts()">
+      <label for="option${i}" class="label-layout">
+        <input
+          type="checkbox"
+          class="custom-checkbox"
+          id="option${i}"
+          value="${element['userData']['name']}"
+          onchange="changeCheckboxColor(${i})"
+        />
+        ${element['userData']['name']}
+      </label>
+      <br />
+      ${createContactInitials(element)}
+    </div>
+  `;
 }
 
 function showCreatedSubtaskHTML(listItemId, element, i) {
-  return `		
-  <div class="subtask-list-container">
-      <li class="subtask-list-item" id="${listItemId}"><input readonly type="text" value="${element}"></li>
-        <div class="edit-delete-container">
-          <img id="edit-button${i}" onclick="correctSubtask(${i})" src="assets/img/icons/add-task/edit.svg" alt="edit">
-          <img src="assets/img/icons/add-task/edit-btn-spacer.svg" alt="spacer">
-    <img onclick="deleteSubtask(${i})" src="assets/img/icons/add-task/delete.svg" alt="delete">
-        </div>
-  </div>
-`;
+	return /*html*/ `		
+    <div class="subtask-list-container">
+        <li class="subtask-list-item" id="${listItemId}"><input readonly type="text" value="${element}"></li>
+          <div class="edit-delete-container">
+            <img id="edit-button${i}" onclick="correctSubtask(${i})" src="assets/img/icons/add-task/edit.svg" alt="edit">
+            <img src="assets/img/icons/add-task/edit-btn-spacer.svg" alt="spacer">
+      <img onclick="deleteSubtask(${i})" src="assets/img/icons/add-task/delete.svg" alt="delete">
+          </div>
+    </div>
+  `;
 }
 
 function showInitialsOfAssignedHTML(color, initials) {
-  return `
-  <div 
-  class="initialsCyrcle"
+	return /*html*/ `
+    <div 
+      class="initialsCyrcle"
       style="background-color: ${color}">
       ${initials}
-</div>
-`;
+    </div>
+  `;
 }
