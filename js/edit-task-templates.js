0d5d5d0931c63e74e978594b617755b483f44a50
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-15 16:09:04 +0200

    refactor: formatting

--- js/edit-task-templates.js
+++ js/edit-task-templates.js
@@ -5,180 +5,162 @@
  */
 function editTaskHTML(task) {
 	return /*html*/ `
-    <div class="overlay-wrapper">
-        <div class="single-task-card">
-					<div class="close-edit-task-btn">
-						<img
-							src="./assets/img/icons/board/close.svg"
-							alt="close"
-							onclick="closeOverlays()" />
-					</div>
-					
-          <div class="title-cont">
-              <span>Title<span class="asterisk">*</span></span>
-              <input
-                id="title-value"
-                class="field-width-height input-borders title--input"
-                name="title"
-                type="text"
-								maxlength="40"
-                value="${task.title}"
-                class="field-width-height-small input-borders title"
-                placeholder="Enter a title"
-                required
-              />
-            </div>
+  <div class="overlay-wrapper">
+	<div class="single-task-card">
+		<div class="close-edit-task-btn">
+			<img
+				src="./assets/img/icons/board/close.svg"
+				alt="close"
+				onclick="closeOverlays()" />
+		</div>
 
+		<div class="title-cont">
+			<span>Title<span class="asterisk">*</span></span>
+			<input
+				id="title-value"
+				class="field-width-height input-borders title--input"
+				name="title"
+				type="text"
+				maxlength="40"
+				value="${task.title}"
+				class="field-width-height-small input-borders title"
+				placeholder="Enter a title"
+				required />
+		</div>
 
-          <!-- description-->
-            <div class="description-cont">
-              <span>Description</span>
-              <textarea
-                name="description-text"
-                id="description"
-                class="field-width-height-small description-height input-borders description"
-                placeholder="Enter a Description"
-								maxlength="140"
-                cols="30"
-                rows="10"
-              >${task.description}</textarea
-              </textarea>
-            </div>
+		<div class="description-cont">
+			<span>Description</span>
+			<textarea
+				name="description-text"
+				id="description"
+				class="field-width-height-small description-height input-borders description"
+				placeholder="Enter a Description"
+				maxlength="140"
+				cols="30"
+				rows="10">${task.description}
+      </textarea>
+		</div>
 
+		<!-- Date -->
+		<div class="date-cont">
+			<span>Due Date<span class="asterisk">*</span></span>
+			<input
+				id="due-date-value"
+				class="field-width-height-small input-borders due-date"
+				name="date"
+				type="text"
+				value="${task.dueDate}"
+				placeholder="yyyy/mm/dd"
+				required
+				onfocus="(this.type='date')"
+				onblur="(this.type='text')" />
+		</div>
 
-          <!-- Date -->
-          <div class="date-cont">
-          <span>Due Date<span class="asterisk">*</span></span>
-          <!-- <div class="field-width-height"> -->
-          <input
-            id="due-date-value"
-            class="field-width-height-small input-borders due-date"
-            name="date"
-            type="text"
-            value="${task.dueDate}"
-            placeholder="yyyy/mm/dd"
-            required
-            onfocus="(this.type='date')"
-            onblur="(this.type='text')"
-          />
-        </div>
+		<!--Prio-->
+		<div class="prio-cont">
+			<span>Prio</span>
+			<div class="priority">
+				<button
+					id="urgent-btn-edit"
+					class="prio-box prio-unset"
+					onclick="changePriorityEdit('urgent')">
+					Urgent
+					<img
+						id="img-urgent-edit"
+						src="./assets/img/icons/add-task/urgent.svg"
+						alt="urgent symbol" />
+				</button>
+				<button
+					id="medium-btn-edit"
+					class="prio-box prio-unset"
+					onclick="changePriorityEdit('medium')">
+					Medium
+					<img
+						id="img-medium-edit"
+						src="./assets/img/icons/add-task/medium-white.svg"
+						alt="medium symbol" />
+				</button>
+				<button
+					id="low-btn-edit"
+					class="prio-box prio-unset"
+					onclick="changePriorityEdit('low')">
+					Low
+					<img
+						id="img-low-edit"
+						src="./assets/img/icons/add-task/low.svg"
+						alt="low symbol" />
+				</button>
+			</div>
+		</div>
 
-          <!--Prio-->
-          <div class="prio-cont">
-              <span>Prio</span>
-              <div class="priority">
-                <button
-                  id="urgent-btn-edit"
-                  class="prio-box prio-unset"
-                  onclick="changePriorityEdit('urgent')"
-                >
-                  Urgent
-                  <img
-                    id="img-urgent-edit"
-                    src="./assets/img/icons/add-task/urgent.svg"
-                    alt="urgent symbol"
-                  />
-                </button>
-                <button
-                  id="medium-btn-edit"
-                  class="prio-box prio-unset"
-                  onclick="changePriorityEdit('medium')"
-                >
-                  Medium
-                  <img
-                    id="img-medium-edit"
-                    src="./assets/img/icons/add-task/medium-white.svg"
-                    alt="medium symbol"
-                  />
-                </button>
-                <button
-                  id="low-btn-edit"
-                  class="prio-box prio-unset"
-                  onclick="changePriorityEdit('low')"
-                >
-                  Low
-                  <img
-                    id="img-low-edit"
-                    src="./assets/img/icons/add-task/low.svg"
-                    alt="low symbol"
-                  />
-                </button>
-              </div>
-            </div>
+		<!-- assign to -->
+		<div class="assign-to-cont">
+			<span>Assigned to</span>
+			<div
+				id="assigned-to"
+				class="field-width-height borders input-borders assigned-to"
+				onclick="openAndCloseDropDownToAssign()">
+				<input
+					name="select-contact"
+					type="text"
+					class="border-none"
+					id="dropdownInput"
+					placeholder="Select contacts to assign"
+					onkeyup="filterContactsToAssignEdit(), onlyOpenDropDownToAssign()" />
+				<img
+					id="arrowImg"
+					src="./assets/img/icons/add-task/arrow-down.svg"
+					alt="Arrow down icon" />
+			</div>
+			<div
+				class="dropdown-content dropdown-content-small btn-borders dropdown-content-style"
+				id="dropdownContent">
+				<div id="labels"></div>
+			</div>
+			<div id="initialsOfAssigned"></div>
+		</div>
 
+		<!-- Subtasks -->
+		<div class="subtask-cont">
+			<span>Subtasks</span>
+			<div class="field-width-height-small input-borders subtasks">
+				<input
+					id="input-of-subtask"
+					class="border-none"
+					type="text"
+					placeholder="Add new subtasks"
+					onfocus="showSubtaskInput()" />
+				<img
+					onclick="showSubtaskInput()"
+					src="./assets/img/icons/add-task/plus.svg"
+					alt="plus"
+					class="add-subtask-btn" />
+				<img
+					onclick="clearSubtaskInput()"
+					src="./assets/img/icons/add-task/close.svg"
+					alt="cancel" />
 
-          <!-- assign to -->
-          <div class="assign-to-cont">
-          <span>Assigned to</span>
-          <div
-            id="assigned-to"
-            class="field-width-height borders input-borders assigned-to"
-            onclick="openAndCloseDropDownToAssign()">
-            <input
-              name="select-contact"
-              type="text"
-              class="border-none"
-              id="dropdownInput"
-              placeholder="Select contacts to assign"
-              onkeyup="filterContactsToAssignEdit(), onlyOpenDropDownToAssign()"
-            />
-            <img
-              id="arrowImg"
-              src="./assets/img/icons/add-task/arrow-down.svg"
-              alt="Arrow down icon"
-            />
-          </div>
-          <div
-            class="dropdown-content dropdown-content-small btn-borders dropdown-content-style"
-            id="dropdownContent"
-          >
-            <div id="labels"></div>
-          </div>
-          <div id="initialsOfAssigned"></div>
-          </div>
-          
-          <!-- Subtasks -->
-      <div class="subtask-cont">
-              <span>Subtasks</span>
-              <div class="field-width-height-small input-borders subtasks">
-                <input
-                  id="input-of-subtask"
-                  class="border-none"
-                  type="text"
-                  placeholder="Add new subtasks"
-                  onfocus="showSubtaskInput()"/>
-                <img
-									onclick="showSubtaskInput()"
-									src="./assets/img/icons/add-task/plus.svg"
-									alt="plus"
-									class="add-subtask-btn" />
-								<img
-									onclick="clearSubtaskInput()"
-									src="./assets/img/icons/add-task/close.svg"
-									alt="cancel" />
+				<img
+					class="subtask-input-spacer"
+					src="./assets/img/icons/add-task/edit-btn-spacer.svg"
+					alt="spacer" />
 
-								<img
-									class="subtask-input-spacer"
-									src="./assets/img/icons/add-task/edit-btn-spacer.svg"
-									alt="spacer" />
-
-								<img
-									onclick="createSubtask()"
-									class="accept-subtask-btn"
-									src="./assets/img/icons/add-task/done.svg"
-									alt="done" />
-              </div>
-              <div id="show-subtasks-container"></div>
-            </div>
-          </div>
-        </div>
-				<div class="ok-btn-cont btn-span-img-ctn">
-					<button class=" btn-borders btn-create"  onclick="exchangeEditedTask()">
-						OK <img src="./assets/img/icons/add-task/check.svg" alt="check" />
-					</button>
-				</div>
-           
-        `;
+				<img
+					onclick="createSubtask()"
+					class="accept-subtask-btn"
+					src="./assets/img/icons/add-task/done.svg"
+					alt="done" />
+			</div>
+			<div id="show-subtasks-container"></div>
+		</div>
+	</div>
+</div>
+<div class="ok-btn-cont btn-span-img-ctn">
+	<button class="btn-borders btn-create" onclick="exchangeEditedTask()">
+		OK <img src="./assets/img/icons/add-task/check.svg" alt="check" />
+	</button>
+</div>
+`;
 }
 
 /**
@@ -215,12 +197,27 @@ function showContactsToEditHTML(i, element) {
 function showSubtasksToEditHTML(listItemId, subtask, index) {
 	return /*html*/ `
     <div class="subtask-list-container">
-      <li id="${listItemId}" class="subtask-list-item"><input readonly type="text" value="${subtask.name}"></li>
-          <div class="edit-delete-container">
-            <img id="edit-button${index}" onclick="correctSubtask(${index})" src="assets/img/icons/add-task/edit.svg" alt="edit">
-            <img src="assets/img/icons/add-task/edit-btn-spacer.svg" alt="spacer">
-            <img onclick="deleteSubtask(${index})" src="assets/img/icons/add-task/delete.svg" alt="delete">
-          </div>
+      <li 
+        id="${listItemId}" 
+        class="subtask-list-item">
+          <input 
+            readonly 
+            type="text" 
+            value="${subtask.name}">
+      </li>
+      <div class="edit-delete-container">
+        <img 
+          id="edit-button${index}" 
+          onclick="correctSubtask(${index})" 
+          src="assets/img/icons/add-task/edit.svg" alt="edit">
+        <img 
+          src="assets/img/icons/add-task/edit-btn-spacer.svg" 
+          alt="spacer">
+        <img 
+          onclick="deleteSubtask(${index})" 
+          src="assets/img/icons/add-task/delete.svg" 
+          alt="delete">
+      </div>
     </div>
       `;
 }
